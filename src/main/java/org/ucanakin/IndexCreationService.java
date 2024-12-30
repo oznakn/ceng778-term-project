285b76318e0314368da563293544ed67efc25e08
ucanyiit <ucanyiit@gmail.com>
2024-12-30 17:19:00 +0300

    Add index creation service

---
+++ src/main/java/org/ucanakin/IndexCreationService.java
@@ -0,0 +1,91 @@
+package org.ucanakin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.FSDirectory;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class IndexCreationService {
+
+  public void createIndex(String indexPath) throws IOException {
+    StandardAnalyzer analyzer = new StandardAnalyzer();
+    IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+    IndexWriter writer = new IndexWriter(FSDirectory.open(Paths.get(indexPath)), indexWriterConfig);
+    final File folder = new File("ft/all");
+    addDocuments(folder, writer);
+    writer.close();
+  }
+
+  private List<Document> parse(final File file) throws ParserConfigurationException, IOException, SAXException {
+    ArrayList<Document> documents = new ArrayList<>();
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setValidating(false);
+    factory.setIgnoringElementContentWhitespace(true);
+    DocumentBuilder builder = factory.newDocumentBuilder();
+
+
+    String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
+    String contentWithArray = "<root>" + content.replace("\n", "") + "</root>";
+    //System.out.println("Reading " + file.getPath());
+    //System.out.println(contentWithArray.substring(0, 100));
+
+    org.w3c.dom.Document doc = builder.parse(new InputSource(new StringReader(contentWithArray)));
+
+    var rootDoc = doc.getFirstChild();
+    for (int i = 0; i < rootDoc.getChildNodes().getLength(); i++) {
+      Document document = new Document();
+      var itemDoc = rootDoc.getChildNodes().item(i);
+      for (int j = 0; j < itemDoc.getChildNodes().getLength(); j++) {
+        var propDoc = itemDoc.getChildNodes().item(j);
+        //System.out.println(propDoc.getNodeName() + " ::: " + propDoc.getTextContent());
+        document.add(new TextField(
+            propDoc.getNodeName(),
+            propDoc.getTextContent(),
+            Field.Store.YES
+        ));
+      }
+      documents.add(document);
+    }
+
+    return documents;
+  }
+
+  private void addDocument(final File file, IndexWriter writer) {
+    try {
+      List<Document> documents = parse(file);
+
+      for (Document document: documents) {
+        writer.addDocument(document);
+      }
+    } catch (Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  private void addDocuments(final File folder, IndexWriter writer) {
+    for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
+      if (fileEntry.isDirectory()) {
+        addDocuments(fileEntry, writer);
+      } else {
+        addDocument(fileEntry, writer);
+      }
+    }
+  }
+
+}
--- src/main/java/org/ucanakin/Main.java
+++ src/main/java/org/ucanakin/Main.java
@@ -1,98 +1,49 @@
 package org.ucanakin;
 
-import com.robrua.nlp.bert.Bert;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.Objects;
+import java.nio.file.Paths;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.StoredFields;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.store.ByteBuffersDirectory;
-import org.apache.lucene.store.Directory;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.store.FSDirectory;
 
 public class Main {
 
+  static IndexCreationService indexCreationService = new IndexCreationService();
+  private static final String INDEX_PATH = "index1";
+
   public static void main(String[] args) {
-    try {
-      Bert bert = Bert.load("com/robrua/nlp/easy-bert/bert-uncased-L-12-H-768-A-12");
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    //try {
+    //  Bert bert = Bert.load("com/robrua/nlp/easy-bert/bert-uncased-L-12-H-768-A-12");
+    //} catch (Exception e) {
+    //  e.printStackTrace();
+    //}
 
     try {
-      String content = "hello world";
+      // createIndex is used to create the index. If index is already created, you don't need to run this.
+      indexCreationService.createIndex(INDEX_PATH);
 
-      Directory memoryIndex = new ByteBuffersDirectory();
-      StandardAnalyzer analyzer = new StandardAnalyzer();
-      IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
-      IndexWriter writer = new IndexWriter(memoryIndex, indexWriterConfig);
-      Document document = new Document();
+      QueryParser parser = new QueryParser("TEXT", new StandardAnalyzer());
+      Query query = parser.parse("International Organized Crime");
 
-      final File folder = new File("ft/all");
-
-      addDocuments(folder, writer);
-
-      document.add(new TextField("content", content, Field.Store.YES));
-
-      writer.addDocument(document);
-      writer.close();
-
-      QueryParser parser = new QueryParser("content", new StandardAnalyzer());
-      Query query = parser.parse("hello");
-      IndexReader reader = DirectoryReader.open(memoryIndex);
+      IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(INDEX_PATH)));
       IndexSearcher searcher = new IndexSearcher(reader);
+      searcher.setSimilarity(new BM25Similarity());
 
       TopDocs results = searcher.search(query, 10);
+      StoredFields storedFields = searcher.storedFields();
       for (ScoreDoc scoreDoc : results.scoreDocs) {
-        System.out.println("Found: " + scoreDoc.score);
+        var doc = storedFields.document(scoreDoc.doc);
+        System.out.println("Found: " + scoreDoc.score + " ::: " + doc.get("DOCNO"));
       }
     } catch (Exception e) {
       e.printStackTrace();
-    }
-  }
-
-
-  private static void addDocument(final File file, IndexWriter writer) {
-    try(BufferedReader br = new BufferedReader(new FileReader(file.getPath()))) {
-      StringBuilder sb = new StringBuilder();
-      String line = br.readLine();
-
-      while (line != null) {
-        sb.append(line);
-        sb.append(System.lineSeparator());
-        line = br.readLine();
-      }
-
-      String allText = sb.toString();
-
-      Document document = new Document();
-      document.add(new TextField(file.getName(), allText, Field.Store.YES));
-      writer.addDocument(document);
-
-      System.out.println(file.getName());
-    } catch (Exception e){
-      e.printStackTrace();
-    }
-  }
-
-  private static void addDocuments(final File folder, IndexWriter writer) {
-    for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
-      if (fileEntry.isDirectory()) {
-        addDocuments(fileEntry, writer);
-      } else {
-        addDocument(fileEntry, writer);
-      }
     }
   }
 }
